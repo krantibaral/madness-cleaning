461313016fe4c2d1f851fbc7bcd71865c2456169
krantiBaral <baralkranti4@gmail.com>
2024-09-16 15:25:35 +0545

    Form updated

--- resources/views/admin/services/form.blade.php
+++ resources/views/admin/services/form.blade.php
@@ -1,33 +1,41 @@
 <div class="form-group row">
+    <!-- Name Field -->
     <div class="col-md-6">
-        <div class="row">
-            <div class="col-md-12">
-                <label for=""> Name *</label>
-                <input type="text" required class="form-control" name="name" value="{{ old('name', $item->name) }}">
-            </div>
-        </div>
+        <label for="name">Name <span class="text-danger">*</span></label>
+        <input type="text" id="name" name="name" class="form-control" 
+               placeholder="Enter name" required 
+               value="{{ old('name', $item->name) }}">
     </div>
+
+    <!-- Location Field -->
     <div class="col-md-6">
-        <div class="row">
-            <div class="col-md-12">
-                <label for=""> Location *</label>
-                <input type="text" required class="form-control" name="location" value="{{ old('location', $item->location) }}">
-            </div>
-        </div>
+        <label for="location">Location <span class="text-danger">*</span></label>
+        <input type="text" id="location" name="location" class="form-control" 
+               placeholder="Enter location" required 
+               value="{{ old('location', $item->location) }}">
     </div>
-    <div class="col-md-6">
-        <label for="">Image</label>
-        <input type="file" class="form-control" accept="image/*" name="image">
+
+    <!-- Image Upload -->
+    <div class="col-md-6 mt-3">
+        <label for="image">Image</label>
+        <input type="file" id="image" name="image" class="form-control" accept="image/*">
         @if ($item->getImage())
-            <img src="{{ $item->getImage() }}" alt="" width="30%">
+            <img src="{{ $item->getImage() }}" alt="Uploaded Image" class="mt-2" style="width: 100%; max-width: 200px; height: auto;">
         @endif
     </div>
-    <div class="col-md-6 mt-4">
-        <label for="">Price *</label>
-        <input type="number" required class="form-control" name="price" value="{{ old('price', $item->price) }}">
+
+    <!-- Price Field -->
+    <div class="col-md-6 mt-3">
+        <label for="price">Price <span class="text-danger">*</span></label>
+        <input type="number" id="price" name="price" class="form-control" 
+               placeholder="Enter price" required 
+               value="{{ old('price', $item->price) }}">
     </div>
-    <div class="col-12 mt-4">
-        <label for="">Description *</label>
-        <textarea name="description" class="form-control" rows="5">{{ old('description', $item->description) }}</textarea>
+
+    <!-- Description Field -->
+    <div class="col-12 mt-3">
+        <label for="description">Description <span class="text-danger">*</span></label>
+        <textarea id="description" name="description" class="form-control" rows="5" 
+                  placeholder="Enter description">{{ old('description', $item->description) }}</textarea>
     </div>
 </div>
