86854a8beb3521ec14ae549fb00ce743e1c79993
krantiBaral <baralkranti4@gmail.com>
2024-09-17 19:51:47 +0545

    Updated

--- resources/views/admin/windows_cleaning_services/create.blade.php
+++ resources/views/admin/windows_cleaning_services/create.blade.php
@@ -2,7 +2,7 @@
 @push('styles')
 @endpush
 @section('form_content')
-    @include('admin.house_cleaning_services.form')
+    @include('admin.windows_cleaning_services.form')
 @endsection
 @push('scripts')
 
