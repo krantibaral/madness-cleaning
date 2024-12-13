100d53fe9422e4e59789ea02d6dccc10fed7bf63
krantiBaral <baralkranti4@gmail.com>
2024-12-13 15:16:06 +0545

    Update

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
+            $table->string('key', 191)->primary()->collation('utf8_general_ci');
             $table->mediumText('value');
             $table->integer('expiration');
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key')->primary();
+            $table->string('key', 191)->primary()->collation('utf8_general_ci');
             $table->string('owner');
             $table->integer('expiration');
         });
